36ef7abfae21d47877dcf46406229d4b934b0993
Torch <suluh.webdevelopers@hotmail.com>
2023-05-08 07:56:56 +0700

    :memo: optimized again dear

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -9,11 +9,14 @@
 <meta name="author" content="<?= settings('author') ?>" />
 
 <link rel="preconnect" href="https://api.fonts.coollabs.io" />
-<link rel="preconnect" href="https://ik.imagekit.io" />
 <link rel="preconnect" href="https://avatars3.githubusercontent.com" />
 <link rel="dns-prefetch" href="https://ik.imagekit.io" />
 <link rel="shortcut icon" href="https://avatars3.githubusercontent.com/u/20874779" type="image/x-icon" />
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/unpoly@3.1.0/unpoly.min.css" />
+<link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="https://cdn.jsdelivr.net/npm/unpoly@3.1.0/unpoly.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/unpoly@3.1.0/unpoly.min.css" />
+</noscript>
 <link rel="stylesheet" href="<?= asset('assets/css/styles.css') ?>" />
--- application/views/includes/script.php
+++ application/views/includes/script.php
@@ -1,5 +1,5 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.8.3/dist/lazyload.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/unpoly@3.1.0/unpoly.min.js"></script>
-<script src="<?= asset('assets/js/script.js') ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" defer=""></script>
+<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" defer=""></script>
+<script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.8.3/dist/lazyload.min.js" defer=""></script>
+<script src="https://cdn.jsdelivr.net/npm/unpoly@3.1.0/unpoly.min.js" defer=""></script>
+<script src="<?= asset('assets/js/script.js') ?>" defer=""></script>
