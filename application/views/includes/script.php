ead7119da26de497eddf24c4cf617bbbbc033995
Torch <20874779+sooluh@users.noreply.github.com>
2023-05-09 11:04:49 +0700

    :wastebasket: get rid jquery

--- application/views/includes/script.php
+++ application/views/includes/script.php
@@ -1,4 +1,3 @@
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js" defer=""></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" defer=""></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" defer=""></script>
 <script src=" https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" defer=""></script>
